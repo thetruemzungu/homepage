0480c619fe5b064bf568c0e3f171225947521059
thetruemzungu <latimer.ethan@gmail.com>
2019-04-25 15:13:12 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@
     get period() {
       var offset = -300;
       var dateFuture = new Date(April 25, 2019 23:30:00);
-      dateFuture = new Date(dateFuture.getTime() + offset*60*1000);
       var dateNow = new Date();
       var seconds = Math.floor((dateFuture - (dateNow))/1000);
       var minutes = Math.floor(seconds/60);
